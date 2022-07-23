f1194b5e9adeda29f0f943031e3e92e8bb07e561
bios2022 <fercerivero@gmail.com>
2022-07-23 08:17:46 -0300

    clase03

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css';
+
+const Navbar = () => { 
+    return (
+        <nav className="Navbar">
+            <div>
+                
+                <h1>Electro Online</h1>
+            </div>
+            <div>
+                <button>Inicio</button>
+                <button>Ofertas Web</button>
+                <button>Tienda</button>    
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
