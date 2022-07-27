e5bcc964f24239e11144a1a0fe552bd12e190ce3
bios2022 <fercerivero@gmail.com>
2022-07-27 19:30:26 -0300

    clase02-JSX

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,18 @@
 import './Navbar.css';
+import CartWidget from '../CartWidget/CartWidget';
 
 const Navbar = () => { 
     return (
         <nav className="Navbar">
             <div>
-                
                 <h1>Electro Online</h1>
             </div>
             <div>
                 <button>Inicio</button>
                 <button>Ofertas Web</button>
-                <button>Tienda</button>    
+                <button>Tienda</button>  
             </div>
+            <CartWidget />
         </nav>
     )
 }
