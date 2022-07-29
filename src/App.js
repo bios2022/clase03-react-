8ab696dc3ca7d325ae99bed8955ae931aa4ef3b0
bios2022 <fercerivero@gmail.com>
2022-07-29 20:13:50 -0300

    clase 05

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Counter from './components/Counter/Counter';
 
 function App() {
+  
+  const handleOnAdd = (cantidad) => {
+    console.log(cantidad )
+  }
+  
+  
+  
+  
+  
   return (
     <div className="App">
         <Navbar />
         <ItemListContainer bienvenida='Aca estan las mejores ofertas'/>
+        <Counter stock={10} onAdd={handleOnAdd} />
     </div>
   );
 }
